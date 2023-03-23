2443a7109b6faeaab603f7ce463198491f33d462
owenv <owenvanmantgem@gmail.com>
2023-03-22 22:23:44 -0500

    All done

--- README.md
+++ README.md
@@ -20,9 +20,10 @@ You can use the escape sequence **\t** to put a tab character between columns of
 5. Write a loop in main that invokes check with the values 0.1, 1.0, 10.0, and 100.0.
 * How does the accuracy of the result vary as x varies?
     * Compare the number of digits of agreement rather than the difference between the actual and estimated values.
-
+        * For 0.1 and 1 it is fine. For 10 and 100 it is completely inaccurate.
 6. Add a loop in main that checks myexp with the values -0.1, -1.0, -10.0, and -100.0.
 * Comment on the accuracy.
+  * like with the first set it works fine for the first two and then goes crazy and becomes completely inaccurate.
 
 ## Part 2 - Submit via GitHub Pull Request
 * Perform the same Pull request steps you've been doing all semester!
--- src/InfiniteSeries.java
+++ src/InfiniteSeries.java
@@ -1,7 +1,46 @@
+
 public class InfiniteSeries {
 
 
+    public static double myexp(double x, double n) {
+        double Xsaver = x;
+
+        double numerator = x;
+        double denominator = 1;
+
+        for (double i = 2; i <= n; i++) {
+                x += (numerator *Xsaver)/(denominator * i);
+                denominator *= i;
+                numerator *= Xsaver;
+        }
+
+        return (x + 1);
+    }
+
+    public  static  void check(double x) {
+        System.out.println(myexp(x, 17));
+        System.out.println(Math.exp(x));
+    }
     public static void main(String[] args) {
+       check(1);
+
+        System.out.println("check 1");
+       double b = 0.01;
+       for (int i = 0; i < 4; i++) {
+           b *= 10;
+           System.out.println(b);
+           check(b);
+           System.out.println("");
+       }
+
+        System.out.println("check 2");
+        b = -0.01;
+        for (int i = 0; i < 4; i++) {
+            b *= 10;
+            System.out.println(b);
+            check(b);
+            System.out.println("");
+        }
 
     }
 }
