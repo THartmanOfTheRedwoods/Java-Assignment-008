f188fba74deff2f586645eb00832203119b4e9d9
sequoyah1 <sequoyahschaefer@yahoo.com>
2023-03-27 02:56:44 -0400

    Sequoyah Kerpan Schaefer Spring 2023 Assignment8

--- src/InfiniteSeries.java
+++ src/InfiniteSeries.java
@@ -1,7 +1,37 @@
-public class InfiniteSeries {
+import java.util.stream.LongStream;
+public class InfiniteSeries{
+    public static int e;
+    public static long factorialUsingStreams(int n) {
+        return LongStream.rangeClosed (1, n)
+                .reduce(1, (long x, long y) -> x * y);
+    }
+    public static double MyExp(double x, int n) {
+        double e = 0.0;
+        double num1 = 1;
+        double num2 = 1;
+
+
+        for (int i = 0; i <= n; i++) {
+            e += Math.pow(x ,i) / factorialUsingStreams(i);
+            e += (num1*=x) / (num2*=i);
+
+
+        }
+        return e;
+
+    }
+
+    public static void check (double x) {
+        int i = 7;
+
+        System.out.println((x+"\t"+MyExp(x, i)+"\t"+Math.exp(x)));
+    }
+
 
 
     public static void main(String[] args) {
 
     }
 }
+
+
